293186bd722c7d4ab3ef1893f71920b151c327e5
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-15 23:01:17 +0530

    connected with firebase but problems

--- login.js
+++ login.js
@@ -1,4 +1,23 @@
 function login()
 {
-    
+    var useremail = document.getElementById("text_email").value;
+    var userpassword= document.getElementById("text_password").value;
+
+    firebase.auth().signInWithEmailAndPassword(useremail, userpassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    window.alert("successfully loged in");
+    window.location="homepage.html";
+  });
 }
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    window.location.href="homepage.html";
+  } else {
+    // No user is signed in.
+    window.alert("err");
+  }
+});
